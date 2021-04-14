b0adaf48b02c363e21e65ff49ca4b8f00562adab
GlubokinV <glubokin_vyacheslav@mail.ru>
2021-04-14 19:53:42 +0700

    Обновлён InputUserData.py

--- Program/InputUserData.py
+++ Program/InputUserData.py
@@ -4,21 +4,6 @@ from re import findall
 class DataInput:
     """Ввод данных пользователем"""
 
-    # def example(self, userInput):
-    #     """Обработка введённых данных пользователя"""
-    #     self.userInput = userInput
-    #     if not self.userInput:
-    #         raise print('Вы ничего не ввели')
-    #     __splitted_text = map(str, userInput)
-    #     inplett = list(__splitted_text)
-    #     fitr = r'[КСЗ]'
-    #     if not findall(fitr, userInput):
-    #         raise print('Нет символов для сортировки')
-    #     # print('результат работы example:', inplett)
-    #     return inplett
-
-
-
     def example(self, userInput):
         """Обработка введённых данных пользователя"""
 
